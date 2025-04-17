8fcec6ddec182702fe8a15362c0326f66720b40d
MuhamadFaizMutalib <muhamad.faiz.mutalib@gmail.com>
2025-04-17 17:55:34 +0800

    Description of your changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const config = require('server/config/config');
+const config = require('./server/config/config');
 
 // Initialize express app
 const app = express();
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './client')));
 
 // Routes
-app.use('/api/auth', require('server/routes/auth.routes'));
+app.use('/api/auth', require('./server/routes/auth.routes'));
 
 // Serve the AngularJS app for any other routes
 app.get('*', (req, res) => {
